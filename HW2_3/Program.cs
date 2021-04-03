d5772ef7ce67a0f8046a313019a5e46a29792b2f
Пользователь <gorobnuak.l@gmail.com>
2021-04-03 14:41:42 +0300

    Добавьте файлы проекта.

---
+++ HW2_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Перерозташувати елементи у масиві(відємні, невідємні) у порядку їх слідування. 
+            //Для масиву : 1  2 - 4 - 55 - 7 10 100
+            //Повинні отримати змінений масив: -4 - 55 - 7  1 2 10 100
+            //Використати FindAll(), CopyTo().
+
+            int[] arr = { 1,  2, - 4, - 55, - 7, 10, 100 };
+            
+            int[] arr2 = Array.FindAll(arr,  j=> j < 0);
+            int[] arr3 = Array.FindAll(arr, j => j > 0);
+            int[] arr4 = new int[arr.Length];
+            arr2.CopyTo(arr4, 0);
+            arr3.CopyTo(arr4, arr2.Length);
+
+            foreach (int i in arr4)
+            {
+                Console.WriteLine(i);
+            }
+        }  
+    }
+}
